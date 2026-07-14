ecefae6abffd0d57f26b2cbcd7989ab72a7393b2
panejii <faneziaja@gmail.com>
2026-07-14 11:26:39 +0700

    feat: add map section and implementing simple mapbox-gl library for layouting

--- Slicing-Dashboard-NTI/src/components/Dashboard/SecondSection/map/MapSection.jsx
+++ Slicing-Dashboard-NTI/src/components/Dashboard/SecondSection/map/MapSection.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import Mapbox from "./Mapbox";
 
 const MapSection = () => {
   return (
-    <div>MapSection</div>
-  )
-}
+    <div className="bg-white rounded-2xl p-4 shadow-sm h-full">
 
-export default MapSection
+      <div className="flex justify-between items-center mb-4">
+        <h2 className="font-semibold text-lg">
+          Regional Map
+        </h2>
+
+        <select className="border rounded-lg px-2 py-1 text-sm">
+          <option>Indonesia</option>
+        </select>
+      </div>
+
+      <div className="h-105">
+        <Mapbox />
+      </div>
+
+    </div>
+  );
+};
+
+export default MapSection;
---
+++ Slicing-Dashboard-NTI/src/components/Dashboard/SecondSection/map/Mapbox.jsx
@@ -0,0 +1,20 @@
+import { useEffect, useRef } from "react";
+import mapboxgl from "mapbox-gl";
+import { Container } from "lucide-react";
+
+mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN;
+
+export default function Map(){
+    const mapRef = useRef(null)
+
+    useEffect(() => {
+        const map = new mapboxgl.Map({
+            container: mapRef.current,
+            center: [78,21], 
+            zoom : 9
+        })
+        return () => map.remove()
+    },[])
+
+    return <div ref={mapRef} style={{width:"100%",height:"420px"}}/>
+};
