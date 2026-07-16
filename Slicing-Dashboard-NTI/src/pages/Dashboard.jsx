27c09b0bbf075997e1d8e920c8c21b65944327dc
panejii <faneziaja@gmail.com>
2026-07-16 15:10:03 +0700

    fix: changing font from poppins to barlow for sidebar and geist for navbar and content

--- Slicing-Dashboard-NTI/src/pages/Dashboard.jsx
+++ Slicing-Dashboard-NTI/src/pages/Dashboard.jsx
@@ -6,7 +6,7 @@
 
     const Dashboard = () => {
         return (
-            <div className="min-h-screen bg-slate-100">
+            <div className="min-h-screen bg-slate-100 ">
 
                 <div className="fixed bottom-2 right-2 z-50 bg-black/80 text-white text-xs font-mono px-2 py-1 rounded">
                     <span className="lg:hidden">base/sm/md</span>
@@ -15,26 +15,32 @@
                     <span className="hidden 2xl:inline">2xl</span>
                 </div>
 
+                <div className='font-barlow'>
+                    <Sidebar />
+                </div>
 
-                <Sidebar />
+                <div className='font-geist'>
+                    <Navbar />
+                    
+                    {/* Chart Section */}
+                    <div className='ml-10 lg:ml-12 xl:ml-14 2xl:ml-16 mt-2  bg-white rounded-l-2xl flex flex-col gap-2'>
 
-                <Navbar />
-                
-                {/* Chart Section */}
-                <div className='ml-10 lg:ml-12 xl:ml-14 2xl:ml-16 mt-2  bg-white rounded-l-2xl flex flex-col gap-2'>
+                        {/* First Section */}
 
-                    {/* First Section */}
+                        <div className=' ml-2 mt-2 bg-slate-100  rounded-xl border border-slate-200'>
+                            <Hero/>
+                        </div>
 
-                    <div className=' ml-2 mt-2 bg-slate-100  rounded-xl border border-slate-200'>
-                        <Hero/>
-                    </div>
+                        {/* Second Section */}
 
-                    {/* Second Section */}
-
-                    <div className=' ml-2  bg-slate-100   rounded-xl border border-slate-200'>
-                        <SecondSection/>
+                        <div className=' ml-2  bg-slate-100   rounded-xl border border-slate-200'>
+                            <SecondSection/>
+                        </div>
                     </div>
                 </div>
+                
+
+                
 
 
 
